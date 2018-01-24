e792c680a61b8cfd20bebfb993eaad2f6946467b
zc <zhangchen40@126.com>
2018-01-24 19:04:43 +0800

    page split

--- routers/craft-essence.js
+++ routers/craft-essence.js
@@ -1,25 +1,45 @@
 var express = require('express')
 var router = express.Router()
 var fs = require('fs')
+var data = JSON.parse(fs.readFileSync('./libs/craft_essence.txt'))
 
 // middleware that is specific to this router
-router.use(function (req, res, next) {
-  console.log('router craft essence ....')
-  next()
-})
+// router.use(function (req, res, next) {
+//   next()
+// })
 
 // define the home page route
-// router.get('/', function (req, res) {
-//   res.send('Birds home page')
-// })
+router.get('/', function (req, res) {
+  res.send('Craft Essence Home Page')
+})
 
 // define the about route
 router.get('/craft-essences', function (req, res) {
 
-  // 读取文件
-  let data = fs.readFileSync('./libs/craft_essence.txt')
+  // console.log(req.query);
 
-  res.send(JSON.parse(data))
+  let _data = data, count = 20;
+
+  if (req.query.atkhp === 'atk') {
+
+  } else if (req.query.atkhp === 'hp') {
+
+  } else if (req.query.atkhp === 'atkhp') {
+
+  }
+
+  if (req.query.cost) {
+
+  }
+
+  if (req.query.index) {
+    let idx = Number(req.query.index);
+    res.send({
+      index: idx,
+      count: count,
+      list: _data.slice(idx, count + idx)
+    })
+  }
 })
 
 module.exports = router
